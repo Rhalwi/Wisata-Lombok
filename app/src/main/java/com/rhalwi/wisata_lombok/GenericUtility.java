07e1932fe2ac2e23104788f366b110926caf7988
rhalwi <rizkahra@gmail.com>
2021-01-13 19:00:00 +0800

    tugas UAS

---
+++ app/src/main/java/com/rhalwi/wisata_lombok/GenericUtility.java
@@ -0,0 +1,8 @@
+package com.rhalwi.wisata_lombok;
+
+import java.text.SimpleDateFormat;
+
+public class GenericUtility {
+    public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd-MMM-yyyy");
+
+}
